6746828dd6ec81985e0f7974d81cd42fbf597b5a
Joel Fredericks <joel.fredericks@kurtosys.com>
2019-02-16 10:53:07 +0200

    Section 3.12

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,15 +1,6 @@
 console.log('Starting app.js:');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes');
-
-const res = notes.addNote();
-console.log(res);
-
-/* console.log(notes.add(3, 4));
-
-const user = os.userInfo();
-
-fs.appendFileSync('greeting.txt', `Hello, ${user.username}, you are ${notes.age}`); */

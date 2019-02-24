77230a0c6be365f6ad67e8e97936501d707f8624
Joel Fredericks <joel.fredericks@kurtosys.com>
2019-02-24 18:12:19 +0200

    Section 3.14

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,5 +2,28 @@ console.log('Starting app.js:');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes');
+
+const argv = yargs.argv;
+const command = argv._[0];
+
+switch (command) {
+    case 'add':
+        notes.addNote(argv.title, argv.body);
+        break;
+    case 'list':
+        notes.getAll();
+        break;
+    case 'read':
+        notes.readNote(argv.title, argv.body);
+        break;
+    case 'delete':
+    notes.deleteNote(argv.title);
+        break;
+
+    default:
+    console.log('Command not recognized');
+        break;
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,6 +1,26 @@
 console.log('Starting notes.js:');
 
-module.exports.addNote = () => {
-	console.log('addNote');
-	return 'New Note';
+const addNote = (title, body) => {
+	console.log('TCL: addNote -> title:', title)
+	console.log('TCL: addNote -> body:', body)
 }
+
+const getAll = () => {
+	console.log('Getting all notes');
+}
+
+const readNote = (title, body) => {
+	console.log('Getting', title);
+	console.log('body:', body);
+}
+
+const deleteNote = (title) => {
+	console.log('Deleting', title);
+}
+
+module.exports = {
+	addNote,
+	getAll,
+	readNote,
+	deleteNote
+};
